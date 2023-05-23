54a70241a8425ad7a5cf80f40659da356683349a
John David Smith <john.smith@learningalliances.net>
2023-05-23 11:28:05 -0700

    update member_type to a character

--- R/member_type.R
+++ R/member_type.R
@@ -6,7 +6,7 @@ NULL
 #' A convenient copy of SDB table "membership"
 #'
 #' @format ## `member_type`
-#' A data frame with 6 rows andmember_type2 columns:
+#' A data frame with 6 rows and 2 columns:
 #' \describe{
 #'   \item{member_type_id}{Member Type ID Number}
 #'   \item{member_type_name}{Member Type Label}
